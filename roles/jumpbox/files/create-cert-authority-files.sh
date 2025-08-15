603fed82ffed9094601518bdb90c5efeefa5e29b
Daniel Tomlinson <dan.tomlinson@proton.me>
2025-08-15 12:58:41 +0100

    Lots of changes to allow root logins from jumpbox to internal machines as instructions require it

---
+++ roles/jumpbox/files/create-cert-authority-files.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# log output
+exec &>>"/var/log/create-cert-authority-files.log"
+
+echo "----------------"
+date
+echo "Creating Certificate Authority Files"
+
+cd /root/kubernetes-the-hard-way
+
+{
+  openssl genrsa -out ca.key 4096
+  openssl req -x509 -new -sha512 -noenc \
+    -key ca.key -days 3653 \
+    -config ca.conf \
+    -out ca.crt
+}
+
+certs=(
+  "admin" "node-0" "node-1"
+  "kube-proxy" "kube-scheduler"
+  "kube-controller-manager"
+  "kube-api-server"
+  "service-accounts"
+)
+
+for i in ${certs[*]}; do
+  openssl genrsa -out "${i}.key" 4096
+
+  openssl req -new -key "${i}.key" -sha256 \
+    -config "ca.conf" -section ${i} \
+    -out "${i}.csr"
+
+  openssl x509 -req -days 3653 -in "${i}.csr" \
+    -copy_extensions copyall \
+    -sha256 -CA "ca.crt" \
+    -CAkey "ca.key" \
+    -CAcreateserial \
+    -out "${i}.crt"
+done
+
+ls -1 *.crt *.key *.csr
+
+echo
+echo "Certificate Authority Files Created"
