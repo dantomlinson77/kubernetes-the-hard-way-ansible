3c37139c4ba3fb74f09e10521d8db0589e752fcb
Daniel Tomlinson <dan.tomlinson@proton.me>
2025-08-20 17:15:01 +0100

    Script for applying cluster config.  Now idempotent.

--- roles/server/files/apply-cluster-config.sh
+++ roles/server/files/apply-cluster-config.sh
@@ -12,5 +12,7 @@ cd /root/
 echo "Applying kube-apiserver-to-kubelet.yaml..."
 kubectl apply -f kube-apiserver-to-kubelet.yaml --kubeconfig admin.kubeconfig
 
+touch cluster-config-applied
+
 echo
 echo "Applying cluster config complete"
